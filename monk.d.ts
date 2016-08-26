cbf912ec23b6917705258f7ef9fca07d26c7b3fc
sfali23 <f.syed.ali@gmail.com>
2016-08-26 10:18:28 -0400

    updated

--- monk.d.ts
+++ monk.d.ts
@@ -3,69 +3,71 @@
 // Definitions by: Syed Farhan Ali - https://github.com/sfali23
 // // Definitions: https://github.com/DefinitelyTyped/Monk
 
+export class Manager {
 
+    /**
+     *
+     * @return Manager
+     */
+    constructor(uri: string, options?: Object, calback?: Object);
+
+    get(collection: string): Collection;
+
+    create(collection: string): Collection;
+
+    close(force?: boolean, callback?: Function): void;
+
+    then(callback: Function): Promise<Manager>;
+
+    catch(callback: Function): Promise<Manager>;
+} // end of interface "Manager"
+
+export class Collection {
+    /**
+     * Name of collection
+     */
+    name: string;
+
+    aggregate(pipeline: Object, options?: Object, callback?: Function): Promise<Object>;
+
+    bulkWrite(operations: Object, options?: Object, callback?: Function): Promise<Object>;
+
+    count(query?: Object, options?: Object, callback?: Function): Promise<Number>;
+
+    distinct(field: string, query?: Object, options?: Object, callback?: Function): Promise<Object>;
+
+    drop(callback?: Function): Promise<Boolean>;
+
+    dropIndex(index: Object, options?: Object, callback?: Function): Promise<Object>;
+
+    dropIndexes(callback?: Function): Promise<Object>;
+
+    index(fieldOrSpec: Object, options?: Object, callback?: Function): Promise<string>;
+
+    ensureIndex(fieldOrSpec: Object, options?: Object, callback?: Function): Promise<string>;
+
+    find(query?: Object, options?: Object, callback?: Function): Promise<Object[]>;
+
+    findOne(query: Object, options?: Object, callback?: Function): Promise<Object>;
+
+    findOneAndDelete(query: Object, options?: Object, callback?: Function): Promise<Object>;
+
+    findOneAndUpdate(query: Object, update: Object, options?: Object, callback?: Function): Promise<Object>;
+
+    group(keys: Object, condition: Object, initial: Object, reduce: Function, finalize?: Function, command?: Boolean, options?: Object, callback?: Object): Promise<Object>;
+
+    indexes(callback?: Function): Promise<Object[]>;
+
+    insert(docs: Object, options?: Object, callback?: Function): Promise<any>;
+
+    remove(query?: Object, options?: Object, callback?: Function): Promise<Object>;
+
+    update(query: Object, update: Object, options?: Object, callback?: Function): Promise<Object[]>;
+} // end of interface "Collector"
 
 declare namespace monk {
-    export class Manager {
 
-        /**
-         *
-         * @return Manager
-         */
-        constructor(uri: string, options?: Object, calback?: Object);
+    export function monk(uri: string, options?: Object, calback?: Object): Manager;
 
-        get(collection: string): Collection;
-
-        create(collection: string): Collection;
-
-        close(force?: boolean, callback?: Function): void;
-
-        then(callback: Function): Promise<Manager>;
-
-        catch(callback: Function): Promise<Manager>;
-    } // end of interface "Manager"
-
-    export class Collection {
-        /**
-         * Name of collection
-         */
-        name: string;
-
-        aggregate(pipeline: Object, options?: Object, callback?: Function): Promise<Object>;
-
-        bulkWrite(operations: Object, options?: Object, callback?: Function): Promise<Object>;
-
-        count(query?: Object, options?: Object, callback?: Function): Promise<Number>;
-
-        distinct(field: string, query?: Object, options?: Object, callback?: Function): Promise<Object>;
-
-        drop(callback?: Function): Promise<Boolean>;
-
-        dropIndex(index: Object, options?: Object, callback?: Function): Promise<Object>;
-
-        dropIndexes(callback?: Function): Promise<Object>;
-
-        index(fieldOrSpec: Object, options?: Object, callback?: Function): Promise<string>;
-
-        ensureIndex(fieldOrSpec: Object, options?: Object, callback?: Function): Promise<string>;
-
-        find(query?: Object, options?: Object, callback?: Function): Promise<Object[]>;
-
-        findOne(query: Object, options?: Object, callback?: Function): Promise<Object>;
-
-        findOneAndDelete(query: Object, options?: Object, callback?: Function): Promise<Object>;
-
-        findOneAndUpdate(query: Object, update: Object, options?: Object, callback?: Function): Promise<Object>;
-
-        group(keys: Object, condition: Object, initial: Object, reduce: Function, finalize?: Function, command?: Boolean, options?: Object, callback?: Object): Promise<Object>;
-
-        indexes(callback?: Function): Promise<Object[]>;
-
-        insert(docs: Object, options?: Object, callback?: Function): Promise<any>;
-
-        remove(query?: Object, options?: Object, callback?: Function): Promise<Object>;
-
-        update(query: Object, update: Object, options?: Object, callback?: Function): Promise<Object[]>;
-    } // end of interface "Collector"
 }
 
